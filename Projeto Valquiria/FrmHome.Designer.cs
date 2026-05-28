c1f150b2ffd7be79ef151ea3020515bdce1b936b
JEFFERSON DE CARVALHO RODRIGUES <jefferson.crodrigue4@senacsp.edu.br>
2026-05-27 21:49:31 -0300

    atualização do lyout

--- Projeto Valquiria/FrmHome.Designer.cs
+++ Projeto Valquiria/FrmHome.Designer.cs
@@ -32,29 +32,32 @@
             btnPedidos = new Button();
             btnDados = new Button();
             txtPesquisar = new TextBox();
-            lblPesquisar = new Label();
             dgvPedidos = new DataGridView();
             btnProdutos = new Button();
+            btnPedido = new Button();
+            btnProduto = new Button();
             ((System.ComponentModel.ISupportInitialize)panelMenu).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dgvPedidos).BeginInit();
             SuspendLayout();
             // 
             // panelMenu
             // 
-            panelMenu.BackColor = Color.White;
+            panelMenu.BackColor = Color.FromArgb(159, 214, 242);
             panelMenu.Dock = DockStyle.Left;
             panelMenu.Location = new Point(0, 0);
             panelMenu.Name = "panelMenu";
-            panelMenu.Size = new Size(273, 791);
+            panelMenu.Size = new Size(273, 694);
             panelMenu.TabIndex = 0;
             panelMenu.TabStop = false;
             // 
             // btnPedidos
             // 
-            btnPedidos.BackColor = Color.FromArgb(247, 157, 189);
-            btnPedidos.Location = new Point(22, 28);
+            btnPedidos.BackColor = Color.FromArgb(164, 92, 218);
+            btnPedidos.FlatStyle = FlatStyle.Popup;
+            btnPedidos.ForeColor = Color.White;
+            btnPedidos.Location = new Point(61, 264);
             btnPedidos.Name = "btnPedidos";
-            btnPedidos.Size = new Size(228, 126);
+            btnPedidos.Size = new Size(158, 47);
             btnPedidos.TabIndex = 0;
             btnPedidos.Text = "Pedidos";
             btnPedidos.UseVisualStyleBackColor = false;
@@ -62,10 +65,12 @@
             // 
             // btnDados
             // 
-            btnDados.BackColor = Color.FromArgb(247, 157, 189);
-            btnDados.Location = new Point(22, 419);
+            btnDados.BackColor = Color.FromArgb(253, 208, 23);
+            btnDados.FlatStyle = FlatStyle.Popup;
+            btnDados.ForeColor = Color.White;
+            btnDados.Location = new Point(61, 403);
             btnDados.Name = "btnDados";
-            btnDados.Size = new Size(228, 126);
+            btnDados.Size = new Size(158, 47);
             btnDados.TabIndex = 2;
             btnDados.Text = "Dados";
             btnDados.UseVisualStyleBackColor = false;
@@ -73,23 +78,12 @@
             // 
             // txtPesquisar
             // 
-            txtPesquisar.Location = new Point(855, 91);
+            txtPesquisar.Location = new Point(570, 98);
             txtPesquisar.Name = "txtPesquisar";
-            txtPesquisar.Size = new Size(523, 23);
+            txtPesquisar.PlaceholderText = "Pesquisar:";
+            txtPesquisar.Size = new Size(726, 23);
             txtPesquisar.TabIndex = 6;
             txtPesquisar.TextChanged += txtPesquisar_TextChanged;
-            // 
-            // lblPesquisar
-            // 
-            lblPesquisar.AutoSize = true;
-            lblPesquisar.BackColor = Color.White;
-            lblPesquisar.BorderStyle = BorderStyle.FixedSingle;
-            lblPesquisar.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPesquisar.Location = new Point(782, 92);
-            lblPesquisar.Name = "lblPesquisar";
-            lblPesquisar.Size = new Size(69, 19);
-            lblPesquisar.TabIndex = 4;
-            lblPesquisar.Text = "Pesquisar:";
             // 
             // dgvPedidos
             // 
@@ -97,35 +91,64 @@
             dgvPedidos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvPedidos.BackgroundColor = Color.FromArgb(217, 217, 217);
             dgvPedidos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvPedidos.Location = new Point(558, 161);
+            dgvPedidos.Location = new Point(539, 139);
             dgvPedidos.Name = "dgvPedidos";
             dgvPedidos.ReadOnly = true;
             dgvPedidos.RowHeadersWidth = 51;
-            dgvPedidos.Size = new Size(1104, 717);
+            dgvPedidos.Size = new Size(779, 522);
             dgvPedidos.TabIndex = 5;
             // 
             // btnProdutos
             // 
             btnProdutos.BackColor = Color.FromArgb(247, 157, 189);
-            btnProdutos.Location = new Point(22, 211);
+            btnProdutos.FlatStyle = FlatStyle.Flat;
+            btnProdutos.ForeColor = Color.White;
+            btnProdutos.Location = new Point(61, 337);
             btnProdutos.Name = "btnProdutos";
-            btnProdutos.Size = new Size(228, 126);
+            btnProdutos.Size = new Size(158, 47);
             btnProdutos.TabIndex = 7;
             btnProdutos.Text = "Produtos";
             btnProdutos.UseVisualStyleBackColor = false;
             btnProdutos.Click += button1_Click_1;
             // 
+            // btnPedido
+            // 
+            btnPedido.BackColor = Color.FromArgb(164, 92, 218);
+            btnPedido.FlatStyle = FlatStyle.Popup;
+            btnPedido.ForeColor = Color.White;
+            btnPedido.Location = new Point(61, 264);
+            btnPedido.Name = "btnPedido";
+            btnPedido.Size = new Size(158, 47);
+            btnPedido.TabIndex = 0;
+            btnPedido.Text = "Pedidos";
+            btnPedido.UseVisualStyleBackColor = false;
+            btnPedido.Click += button1_Click;
+            // 
+            // btnProduto
+            // 
+            btnProduto.BackColor = Color.FromArgb(56, 206, 22);
+            btnProduto.FlatStyle = FlatStyle.Popup;
+            btnProduto.ForeColor = Color.White;
+            btnProduto.Location = new Point(61, 337);
+            btnProduto.Name = "btnProduto";
+            btnProduto.Size = new Size(158, 47);
+            btnProduto.TabIndex = 7;
+            btnProduto.Text = "Produtos";
+            btnProduto.UseVisualStyleBackColor = false;
+            btnProduto.Click += button1_Click_1;
+            // 
             // panelConteudo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(52, 174, 250);
-            ClientSize = new Size(1424, 791);
+            BackColor = Color.FromArgb(249, 235, 190);
+            ClientSize = new Size(1345, 694);
+            Controls.Add(btnProduto);
             Controls.Add(btnProdutos);
             Controls.Add(dgvPedidos);
-            Controls.Add(lblPesquisar);
             Controls.Add(txtPesquisar);
             Controls.Add(btnDados);
+            Controls.Add(btnPedido);
             Controls.Add(btnPedidos);
             Controls.Add(panelMenu);
             Name = "panelConteudo";
@@ -145,8 +168,9 @@
         private Button btnPedidos;
         private Button btnDados;
         private TextBox txtPesquisar;
-        private Label lblPesquisar;
         private DataGridView dgvPedidos;
         private Button btnProdutos;
+        private Button btnPedido;
+        private Button btnProduto;
     }
 }
--- Projeto Valquiria/FrmProdutos.Designer.cs
+++ Projeto Valquiria/FrmProdutos.Designer.cs
@@ -29,77 +29,89 @@
         private void InitializeComponent()
         {
             btnVoltar = new Button();
+            dgvDadosProdutos = new DataGridView();
             label1 = new Label();
-            txtNome = new TextBox();
             label2 = new Label();
+            txtNome = new TextBox();
             txtValor = new TextBox();
             btnCadastroProduto = new Button();
             panel1 = new Panel();
-            dgvDadosProdutos = new DataGridView();
-            panel1.SuspendLayout();
+            panel2 = new Panel();
+            btnPedido = new Button();
+            btnProduto = new Button();
+            btnCadastroCliente = new Button();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvDadosProdutos).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // btnVoltar
             // 
             btnVoltar.AccessibleRole = AccessibleRole.Alert;
-            btnVoltar.Location = new Point(14, 44);
-            btnVoltar.Margin = new Padding(3, 4, 3, 4);
+            btnVoltar.Location = new Point(58, 451);
             btnVoltar.Name = "btnVoltar";
-            btnVoltar.Size = new Size(154, 93);
+            btnVoltar.Size = new Size(135, 70);
             btnVoltar.TabIndex = 0;
             btnVoltar.Text = "Voltar";
             btnVoltar.UseVisualStyleBackColor = true;
             btnVoltar.Click += btnVoltar_Click;
             // 
+            // dgvDadosProdutos
+            // 
+            dgvDadosProdutos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDadosProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDadosProdutos.Location = new Point(335, 103);
+            dgvDadosProdutos.Name = "dgvDadosProdutos";
+            dgvDadosProdutos.RowHeadersWidth = 51;
+            dgvDadosProdutos.Size = new Size(998, 463);
+            dgvDadosProdutos.TabIndex = 5;
+            // 
             // label1
             // 
             label1.BackColor = Color.White;
             label1.Font = new Font("Segoe UI", 10F);
-            label1.Location = new Point(211, 753);
+            label1.Location = new Point(648, 582);
             label1.Name = "label1";
-            label1.Size = new Size(142, 33);
+            label1.Size = new Size(124, 25);
             label1.TabIndex = 1;
             label1.Text = "Nome do produto";
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(661, 615);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 22);
+            label2.TabIndex = 1;
+            label2.Text = "Valor do Produto";
             // 
             // txtNome
             // 
             txtNome.BorderStyle = BorderStyle.FixedSingle;
             txtNome.Cursor = Cursors.IBeam;
             txtNome.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtNome.Location = new Point(384, 753);
-            txtNome.Margin = new Padding(3, 4, 3, 4);
+            txtNome.Location = new Point(799, 582);
             txtNome.Name = "txtNome";
-            txtNome.Size = new Size(955, 29);
+            txtNome.Size = new Size(335, 25);
             txtNome.TabIndex = 2;
-            // 
-            // label2
-            // 
-            label2.BackColor = Color.White;
-            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(226, 797);
-            label2.Name = "label2";
-            label2.Size = new Size(127, 29);
-            label2.TabIndex = 1;
-            label2.Text = "Valor do Produto";
             // 
             // txtValor
             // 
             txtValor.BorderStyle = BorderStyle.FixedSingle;
             txtValor.Cursor = Cursors.IBeam;
             txtValor.Font = new Font("Segoe UI", 10F);
-            txtValor.Location = new Point(384, 795);
-            txtValor.Margin = new Padding(3, 4, 3, 4);
+            txtValor.Location = new Point(799, 613);
             txtValor.Name = "txtValor";
-            txtValor.Size = new Size(955, 30);
+            txtValor.Size = new Size(335, 25);
             txtValor.TabIndex = 2;
             // 
             // btnCadastroProduto
             // 
-            btnCadastroProduto.Location = new Point(1171, 13);
-            btnCadastroProduto.Margin = new Padding(3, 4, 3, 4);
+            btnCadastroProduto.Location = new Point(550, 10);
             btnCadastroProduto.Name = "btnCadastroProduto";
-            btnCadastroProduto.Size = new Size(150, 56);
+            btnCadastroProduto.Size = new Size(131, 42);
             btnCadastroProduto.TabIndex = 3;
             btnCadastroProduto.Text = "Cadastrar Produto";
             btnCadastroProduto.UseVisualStyleBackColor = true;
@@ -109,43 +121,91 @@
             // 
             panel1.BackColor = Color.White;
             panel1.Controls.Add(btnCadastroProduto);
-            panel1.Location = new Point(175, 740);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(616, 572);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1325, 156);
+            panel1.Size = new Size(696, 78);
             panel1.TabIndex = 4;
             // 
-            // dgvDadosProdutos
+            // panel2
             // 
-            dgvDadosProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvDadosProdutos.Location = new Point(175, 44);
-            dgvDadosProdutos.Margin = new Padding(3, 4, 3, 4);
-            dgvDadosProdutos.Name = "dgvDadosProdutos";
-            dgvDadosProdutos.RowHeadersWidth = 51;
-            dgvDadosProdutos.Size = new Size(1325, 688);
-            dgvDadosProdutos.TabIndex = 5;
+            panel2.BackColor = Color.FromArgb(159, 214, 242);
+            panel2.Controls.Add(btnCadastroCliente);
+            panel2.Controls.Add(btnProduto);
+            panel2.Controls.Add(btnPedido);
+            panel2.Controls.Add(btnVoltar);
+            panel2.Location = new Point(-7, -11);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 934);
+            panel2.TabIndex = 6;
+            // 
+            // btnPedido
+            // 
+            btnPedido.BackColor = Color.FromArgb(164, 92, 218);
+            btnPedido.FlatStyle = FlatStyle.Popup;
+            btnPedido.ForeColor = Color.White;
+            btnPedido.Location = new Point(58, 223);
+            btnPedido.Name = "btnPedido";
+            btnPedido.Size = new Size(135, 70);
+            btnPedido.TabIndex = 0;
+            btnPedido.Text = "Pedidos";
+            btnPedido.UseVisualStyleBackColor = false;
+            // 
+            // btnProduto
+            // 
+            btnProduto.BackColor = Color.FromArgb(56, 206, 22);
+            btnProduto.FlatStyle = FlatStyle.Popup;
+            btnProduto.ForeColor = Color.White;
+            btnProduto.Location = new Point(58, 299);
+            btnProduto.Name = "btnProduto";
+            btnProduto.Size = new Size(135, 70);
+            btnProduto.TabIndex = 1;
+            btnProduto.Text = "Produtos";
+            btnProduto.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastroCliente
+            // 
+            btnCadastroCliente.BackColor = Color.FromArgb(253, 208, 23);
+            btnCadastroCliente.FlatStyle = FlatStyle.Flat;
+            btnCadastroCliente.Location = new Point(58, 375);
+            btnCadastroCliente.Name = "btnCadastroCliente";
+            btnCadastroCliente.Size = new Size(135, 70);
+            btnCadastroCliente.TabIndex = 2;
+            btnCadastroCliente.Text = "Cliente";
+            btnCadastroCliente.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.White;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(661, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(372, 44);
+            label3.TabIndex = 7;
+            label3.Text = "Área de Cadastro de Produtos";
             // 
             // FrmProdutos
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(205, 226, 176);
-            ClientSize = new Size(1537, 925);
+            ClientSize = new Size(1345, 694);
+            Controls.Add(label3);
             Controls.Add(dgvDadosProdutos);
             Controls.Add(txtValor);
             Controls.Add(txtNome);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(btnVoltar);
             Controls.Add(panel1);
-            Margin = new Padding(3, 4, 3, 4);
+            Controls.Add(panel2);
             Name = "FrmProdutos";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Produtos";
             WindowState = FormWindowState.Maximized;
             Load += Pedidos_Load;
-            panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dgvDadosProdutos).EndInit();
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -153,12 +213,17 @@
         #endregion
 
         private Button btnVoltar;
+        private DataGridView dgvDadosProdutos;
         private Label label1;
-        private TextBox txtNome;
         private Label label2;
+        private TextBox txtNome;
         private TextBox txtValor;
         private Button btnCadastroProduto;
         private Panel panel1;
-        private DataGridView dgvDadosProdutos;
+        private Panel panel2;
+        private Button btnCadastroCliente;
+        private Button btnProduto;
+        private Button btnPedido;
+        private Label label3;
     }
 }
