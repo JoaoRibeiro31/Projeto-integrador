e907b94340d9496c3c518569d4d7b317344730fe
João Vitor <jjoaovvitoreducacional999@gmail.com>
2026-05-29 21:49:42 -0300

    FrmClientes finalizado

--- Projeto Valquiria/FrmClientes.Designer.cs
+++ Projeto Valquiria/FrmClientes.Designer.cs
@@ -59,31 +59,34 @@
             label1.BorderStyle = BorderStyle.FixedSingle;
             label1.FlatStyle = FlatStyle.Flat;
             label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(642, 19);
+            label1.Location = new Point(734, 25);
             label1.Name = "label1";
-            label1.Size = new Size(361, 50);
+            label1.Size = new Size(412, 66);
             label1.TabIndex = 0;
             label1.Text = "Área de cadastro de clientes";
             // 
             // txtNome
             // 
-            txtNome.Location = new Point(76, 11);
+            txtNome.Location = new Point(87, 15);
+            txtNome.Margin = new Padding(3, 4, 3, 4);
             txtNome.Name = "txtNome";
-            txtNome.Size = new Size(661, 23);
+            txtNome.Size = new Size(755, 27);
             txtNome.TabIndex = 1;
             // 
             // txtContato
             // 
-            txtContato.Location = new Point(76, 62);
+            txtContato.Location = new Point(87, 83);
+            txtContato.Margin = new Padding(3, 4, 3, 4);
             txtContato.Name = "txtContato";
-            txtContato.Size = new Size(535, 23);
+            txtContato.Size = new Size(611, 27);
             txtContato.TabIndex = 1;
             // 
             // btnCliente
             // 
-            btnCliente.Location = new Point(631, 69);
+            btnCliente.Location = new Point(721, 92);
+            btnCliente.Margin = new Padding(3, 4, 3, 4);
             btnCliente.Name = "btnCliente";
-            btnCliente.Size = new Size(114, 23);
+            btnCliente.Size = new Size(130, 31);
             btnCliente.TabIndex = 2;
             btnCliente.Text = "Cadastrar Cliente";
             btnCliente.UseVisualStyleBackColor = true;
@@ -93,9 +96,9 @@
             // 
             lblNome.AutoSize = true;
             lblNome.BackColor = Color.White;
-            lblNome.Location = new Point(433, 568);
+            lblNome.Location = new Point(495, 757);
             lblNome.Name = "lblNome";
-            lblNome.Size = new Size(43, 15);
+            lblNome.Size = new Size(53, 20);
             lblNome.TabIndex = 4;
             lblNome.Text = "Nome:";
             // 
@@ -103,9 +106,9 @@
             // 
             lblContato.AutoSize = true;
             lblContato.BackColor = Color.White;
-            lblContato.Location = new Point(423, 623);
+            lblContato.Location = new Point(483, 831);
             lblContato.Name = "lblContato";
-            lblContato.Size = new Size(53, 15);
+            lblContato.Size = new Size(65, 20);
             lblContato.TabIndex = 5;
             lblContato.Text = "Contato:";
             // 
@@ -115,19 +118,21 @@
             pnlCadastro.Controls.Add(btnCliente);
             pnlCadastro.Controls.Add(txtNome);
             pnlCadastro.Controls.Add(txtContato);
-            pnlCadastro.Location = new Point(406, 554);
+            pnlCadastro.Location = new Point(464, 739);
+            pnlCadastro.Margin = new Padding(3, 4, 3, 4);
             pnlCadastro.Name = "pnlCadastro";
-            pnlCadastro.Size = new Size(772, 110);
+            pnlCadastro.Size = new Size(882, 147);
             pnlCadastro.TabIndex = 6;
             // 
             // dvgTabela
             // 
             dvgTabela.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dvgTabela.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dvgTabela.Location = new Point(352, 140);
+            dvgTabela.Location = new Point(402, 187);
+            dvgTabela.Margin = new Padding(3, 4, 3, 4);
             dvgTabela.Name = "dvgTabela";
             dvgTabela.RowHeadersWidth = 51;
-            dvgTabela.Size = new Size(917, 406);
+            dvgTabela.Size = new Size(1048, 541);
             dvgTabela.TabIndex = 7;
             // 
             // panelMenu
@@ -135,8 +140,9 @@
             panelMenu.BackColor = Color.FromArgb(159, 214, 242);
             panelMenu.Dock = DockStyle.Left;
             panelMenu.Location = new Point(0, 0);
+            panelMenu.Margin = new Padding(3, 4, 3, 4);
             panelMenu.Name = "panelMenu";
-            panelMenu.Size = new Size(273, 694);
+            panelMenu.Size = new Size(312, 925);
             panelMenu.TabIndex = 8;
             panelMenu.TabStop = false;
             // 
@@ -147,9 +153,10 @@
             btnHome.BackgroundImageLayout = ImageLayout.Stretch;
             btnHome.FlatStyle = FlatStyle.Popup;
             btnHome.ForeColor = Color.White;
-            btnHome.Location = new Point(12, 25);
+            btnHome.Location = new Point(14, 33);
+            btnHome.Margin = new Padding(3, 4, 3, 4);
             btnHome.Name = "btnHome";
-            btnHome.Size = new Size(53, 56);
+            btnHome.Size = new Size(61, 75);
             btnHome.TabIndex = 3;
             btnHome.UseVisualStyleBackColor = false;
             btnHome.Click += btnVoltar_Click;
@@ -159,9 +166,10 @@
             btnProduto.BackColor = Color.FromArgb(56, 206, 22);
             btnProduto.FlatStyle = FlatStyle.Popup;
             btnProduto.ForeColor = Color.White;
-            btnProduto.Location = new Point(61, 303);
+            btnProduto.Location = new Point(70, 404);
+            btnProduto.Margin = new Padding(3, 4, 3, 4);
             btnProduto.Name = "btnProduto";
-            btnProduto.Size = new Size(158, 47);
+            btnProduto.Size = new Size(181, 63);
             btnProduto.TabIndex = 11;
             btnProduto.Text = "Produtos";
             btnProduto.UseVisualStyleBackColor = false;
@@ -171,9 +179,10 @@
             btnClientes.BackColor = Color.FromArgb(253, 208, 23);
             btnClientes.Cursor = Cursors.No;
             btnClientes.ForeColor = Color.White;
-            btnClientes.Location = new Point(61, 356);
+            btnClientes.Location = new Point(70, 475);
+            btnClientes.Margin = new Padding(3, 4, 3, 4);
             btnClientes.Name = "btnClientes";
-            btnClientes.Size = new Size(158, 47);
+            btnClientes.Size = new Size(181, 63);
             btnClientes.TabIndex = 10;
             btnClientes.Text = "Cliente";
             btnClientes.UseVisualStyleBackColor = false;
@@ -183,9 +192,10 @@
             btnPedido.BackColor = Color.FromArgb(164, 92, 218);
             btnPedido.FlatStyle = FlatStyle.Popup;
             btnPedido.ForeColor = Color.White;
-            btnPedido.Location = new Point(61, 250);
+            btnPedido.Location = new Point(70, 333);
+            btnPedido.Margin = new Padding(3, 4, 3, 4);
             btnPedido.Name = "btnPedido";
-            btnPedido.Size = new Size(158, 47);
+            btnPedido.Size = new Size(181, 63);
             btnPedido.TabIndex = 9;
             btnPedido.Text = "Pedidos";
             btnPedido.UseVisualStyleBackColor = false;
@@ -195,9 +205,10 @@
             button1.BackColor = Color.FromArgb(164, 92, 218);
             button1.FlatStyle = FlatStyle.Popup;
             button1.ForeColor = Color.White;
-            button1.Location = new Point(61, 250);
+            button1.Location = new Point(70, 333);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(158, 47);
+            button1.Size = new Size(181, 63);
             button1.TabIndex = 9;
             button1.Text = "Pedidos";
             button1.UseVisualStyleBackColor = false;
@@ -208,9 +219,10 @@
             button2.BackColor = Color.FromArgb(56, 206, 22);
             button2.FlatStyle = FlatStyle.Popup;
             button2.ForeColor = Color.White;
-            button2.Location = new Point(61, 303);
+            button2.Location = new Point(70, 404);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(158, 47);
+            button2.Size = new Size(181, 63);
             button2.TabIndex = 11;
             button2.Text = "Produtos";
             button2.UseVisualStyleBackColor = false;
@@ -218,46 +230,52 @@
             // 
             // txtPesquisar
             // 
-            txtPesquisar.Location = new Point(417, 94);
+            txtPesquisar.Location = new Point(477, 125);
+            txtPesquisar.Margin = new Padding(3, 4, 3, 4);
             txtPesquisar.Name = "txtPesquisar";
             txtPesquisar.PlaceholderText = "Pesquisar:";
-            txtPesquisar.Size = new Size(726, 23);
+            txtPesquisar.Size = new Size(829, 27);
             txtPesquisar.TabIndex = 18;
+            txtPesquisar.TextChanged += txtPesquisar_TextChanged;
             // 
             // btnEdicao
             // 
             btnEdicao.BackgroundImage = (Image)resources.GetObject("btnEdicao.BackgroundImage");
             btnEdicao.FlatStyle = FlatStyle.Flat;
-            btnEdicao.Location = new Point(1315, 140);
+            btnEdicao.Location = new Point(1503, 187);
+            btnEdicao.Margin = new Padding(3, 4, 3, 4);
             btnEdicao.Name = "btnEdicao";
-            btnEdicao.Size = new Size(57, 61);
+            btnEdicao.Size = new Size(65, 81);
             btnEdicao.TabIndex = 19;
             btnEdicao.UseVisualStyleBackColor = true;
+            btnEdicao.Click += btnEdicao_Click;
             // 
             // btnAtualizar
             // 
-            btnAtualizar.Location = new Point(1317, 240);
+            btnAtualizar.Location = new Point(1505, 320);
+            btnAtualizar.Margin = new Padding(3, 4, 3, 4);
             btnAtualizar.Name = "btnAtualizar";
-            btnAtualizar.Size = new Size(75, 23);
+            btnAtualizar.Size = new Size(86, 31);
             btnAtualizar.TabIndex = 20;
             btnAtualizar.Text = "Atualizar";
             btnAtualizar.UseVisualStyleBackColor = true;
             // 
             // btnDeletar
             // 
-            btnDeletar.Location = new Point(1315, 269);
+            btnDeletar.Location = new Point(1503, 359);
+            btnDeletar.Margin = new Padding(3, 4, 3, 4);
             btnDeletar.Name = "btnDeletar";
-            btnDeletar.Size = new Size(75, 23);
+            btnDeletar.Size = new Size(86, 31);
             btnDeletar.TabIndex = 20;
             btnDeletar.Text = "Deletar";
             btnDeletar.UseVisualStyleBackColor = true;
             // 
             // FrmClientes
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 226, 100);
-            ClientSize = new Size(1432, 694);
+            ClientSize = new Size(1637, 925);
             Controls.Add(btnDeletar);
             Controls.Add(btnAtualizar);
             Controls.Add(btnEdicao);
@@ -274,6 +292,7 @@
             Controls.Add(label1);
             Controls.Add(pnlCadastro);
             Controls.Add(panelMenu);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmClientes";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Clientes";
--- Projeto Valquiria/FrmClientes.cs
+++ Projeto Valquiria/FrmClientes.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
@@ -13,8 +7,6 @@ namespace Projeto_Valquiria
 {
     public partial class FrmClientes : Form
     {
-
-        MySqlConnection Conexao;
         string conexao = "Server=localhost;Database=bd_pjval;Uid=root;Pwd=;";
 
         public FrmClientes()
@@ -36,25 +28,22 @@ namespace Projeto_Valquiria
             try
             {
                 conn.Open();
-
-                string sql = @"select nome               AS Nome,
-                                      contato            AS Contato,
-	                                  data_de_cadastro   AS Cadastro
-                                from clientes
-                                ORDER BY data_de_cadastro desc;";
+                string sql = @"SELECT nome AS Nome,
+                                      contato AS Contato,
+                                      data_de_cadastro AS Cadastro
+                               FROM clientes
+                               ORDER BY data_de_cadastro DESC;";
 
                 MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
                 DataTable tabela = new DataTable();
-
                 adapter.Fill(tabela);
 
                 dvgTabela.DataSource = tabela;
             }
             catch (Exception erro)
             {
-                MessageBox.Show("Erro:" + erro.Message);
+                MessageBox.Show("Erro ao carregar clientes: " + erro.Message);
             }
-
             finally
             {
                 conn.Close();
@@ -64,32 +53,43 @@ namespace Projeto_Valquiria
         private void FrmClientes_Load(object sender, EventArgs e)
         {
             CarregarDadosClientes();
+
+            // 🔄 Botões começam invisíveis
+            btnAtualizar.Visible = false;
+            btnDeletar.Visible = false;
+
+            // tabela começa bloqueada
+            dvgTabela.ReadOnly = true;
         }
+
 
         private void btnCliente_Click(object sender, EventArgs e)
         {
+            // Validação dos campos obrigatórios
+            if (string.IsNullOrWhiteSpace(txtNome.Text) || string.IsNullOrWhiteSpace(txtContato.Text))
+            {
+                MessageBox.Show("Preencha todos os campos antes de cadastrar o cliente!",
+                                "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             MySqlConnection conn = new MySqlConnection(conexao);
 
             try
             {
                 conn.Open();
-
-                string sql = @"INSERT INTO clientes (nome, contato) 
-                                      VALUES  (@nome, @contato)";
-
+                string sql = @"INSERT INTO clientes (nome, contato) VALUES (@nome, @contato)";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
-
                 cmd.Parameters.AddWithValue("@nome", txtNome.Text);
                 cmd.Parameters.AddWithValue("@contato", txtContato.Text);
-
                 cmd.ExecuteNonQuery();
-            }
 
+                MessageBox.Show("Cliente cadastrado com sucesso!");
+            }
             catch (Exception erro)
             {
-                MessageBox.Show("Erro:" + erro.Message);
+                MessageBox.Show("Erro ao cadastrar cliente: " + erro.Message);
             }
-
             finally
             {
                 conn.Close();
@@ -97,11 +97,9 @@ namespace Projeto_Valquiria
 
             CarregarDadosClientes();
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+            // 🔄 Limpa os campos após cadastro
+            txtNome.Clear();
+            txtContato.Clear();
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -117,5 +115,51 @@ namespace Projeto_Valquiria
             tela.ShowDialog();
             this.Close();
         }
+
+        private void txtPesquisar_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string filtro = txtPesquisar.Text.Replace("'", "''");
+
+                if (string.IsNullOrWhiteSpace(filtro))
+                {
+                    (dvgTabela.DataSource as DataTable).DefaultView.RowFilter = "";
+                }
+                else
+                {
+                    (dvgTabela.DataSource as DataTable).DefaultView.RowFilter =
+                        $"Nome LIKE '%{filtro}%' OR " +
+                        $"Contato LIKE '%{filtro}%' OR " +
+                        $"Convert(Cadastro, 'System.String') LIKE '%{filtro}%'";
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao aplicar filtro de pesquisa: " + erro.Message);
+            }
+        }
+
+        private bool editando = false; // variável de controle
+
+        private void btnEdicao_Click(object sender, EventArgs e)
+        {
+            if (!editando)
+            {
+                dvgTabela.ReadOnly = false;   // libera edição da tabela
+                btnAtualizar.Visible = true;  // mostra botão atualizar
+                btnDeletar.Visible = true;    // mostra botão deletar
+                editando = true;
+            }
+            else
+            {
+                dvgTabela.ReadOnly = true;    // bloqueia edição novamente
+                btnAtualizar.Visible = false; // esconde botão atualizar
+                btnDeletar.Visible = false;   // esconde botão deletar
+                editando = false;
+            }
+        }
+
     }
 }
+
